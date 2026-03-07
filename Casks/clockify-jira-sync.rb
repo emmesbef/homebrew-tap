959dd004ea79f825ec6f8d3d6754da525f958b20
Fabian <9155770+emmesbef@users.noreply.github.com>
2026-03-07 21:01:23 +0000

    chore: bump clockify-jira-sync to 1.8.1

--- Casks/clockify-jira-sync.rb
+++ Casks/clockify-jira-sync.rb
@@ -1,6 +1,6 @@
 cask "clockify-jira-sync" do
-  version "1.8.0"
-  sha256 "a8116a7befcf84b704793354f06b54ed2f121248a9fffc144066d2207faa0abf"
+  version "1.8.1"
+  sha256 "6c216291c736a0b0db88d3657f9d6f59b2f758c3fa52cd98d7ef5e990268eea3"
 
   url "https://github.com/emmesbef/clockify-jira-sync/releases/download/v#{version}/clockify-jira-sync-v#{version}-macos-universal.zip"
   name "Clockify Jira Sync"
