69082e3415f8b85bb782fdbe21b77b12cad41c5f
Fabian <9155770+emmesbef@users.noreply.github.com>
2026-03-13 20:47:46 +0100

    chore: bump jirafy-clockwork cask to 1.10.22

--- Casks/jirafy-clockwork.rb
+++ Casks/jirafy-clockwork.rb
@@ -1,17 +1,17 @@
 cask "jirafy-clockwork" do
-  version "1.10.19"
-  sha256 "a0f56b723d45e133cbebe60e5a018d6690a6f0a60bc4d2ea46100e729b3dbadd"
+  version "1.10.22"
+  sha256 "b02106b8e56ce7d89fba9193cb856db80d7ec1dfb872cb0e7815c920db60955a"
 
   url "https://gitlab.com/level-87/clockify-jira-sync/-/raw/main/downloads/jirafy-clockwork-v#{version}-macos-universal.zip"
   name "JiraFy Clockwork"
   desc "Desktop app to sync Clockify time entries with Jira worklogs"
   homepage "https://level-87.gitlab.io/"
 
-  app "JiraFy Clockwork.app"
+  app "jirafy-clockwork.app"
 
   postflight do
     system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/JiraFy Clockwork.app"]
+                   args: ["-cr", "#{appdir}/jirafy-clockwork.app"]
   end
 
   zap trash: [
